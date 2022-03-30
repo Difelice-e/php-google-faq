6436a8215fe870b64ba0dc294b77ad9a059cca1e
Difelice-e <difelice.emanuele@gmail.com>
2022-03-30 16:25:43 +0200

    navbar

--- index.php
+++ index.php
@@ -103,6 +103,13 @@
         </div>
         <nav>
             <!-- qua va la navigazione  -->
+            <ul class="navbar-link">
+                <li>Introduzione</li>
+                <li>Norme sulla privacy</li>
+                <li>Termini di servizio</li>
+                <li>Tecnologie</li>
+                <li class="active">Domande frequenti</li>
+            </ul>
         </nav>
     </header>
 
